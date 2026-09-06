7ac9e05d57ba3ad7ec9177712e923fb402904deb
dojo-rommaka <dojo.rommaka@gmail.com>
2026-09-06 12:28:45 -0300

    Add DojoData object with vocabulary entries

---
+++ data.js
@@ -0,0 +1,7 @@
+const DojoData = {
+  palavras: [
+    { palavra: "Think", ipa: "/θɪŋk/", fonema: "/θ/", status: "dificil", nota: "Língua entre os dentes" },
+    { palavra: "See", ipa: "/siː/", fonema: "/iː/", status: "treinar", nota: "Sorriso tenso" },
+    { palavra: "Cat", ipa: "/kæt/", fonema: "/æ/", status: "dominado", nota: "Abertura máxima da boca" }
+  ]
+};
